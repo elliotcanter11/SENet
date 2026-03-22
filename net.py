e1ccc9041fd6da5e7644015b9f37ab9911e98678
Elliot <elliot.canter@cooper.edu>
2026-03-22 13:17:28 -0400

    reconstruct RGB only

--- net.py
+++ net.py
@@ -150,7 +150,7 @@ class MaskedAutoencoderViT(nn.Module):
             for i in range(decoder_depth)])
 
         self.decoder_norm = norm_layer(decoder_embed_dim)
-        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size**2 * in_chans, bias=True) # decoder to patch
+        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size**2 * 3, bias=True) # decoder to patch
         # --------------------------------------------------------------------------
 
 
